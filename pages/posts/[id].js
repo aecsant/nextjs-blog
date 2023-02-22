72b0caa3126190a6b1eda8439bba628aead488ff
santosh kumar <aecsant@gmail.com>
2023-02-22 15:29:51 +0530

    new changes

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+// Add this import
+import Head from 'next/head';
+// Add this import
+import Date from '../../components/date';
+
+
+export default function Post({ postData }) {
+   return (
+     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+       {postData.title}
+       <br />
+       {postData.id}
+       <br />
+       <Date dateString={postData.date} />
+       <br />
+       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+     </Layout>
+   );
+ }
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
